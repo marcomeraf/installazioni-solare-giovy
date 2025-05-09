c84c6bb8723760597efd7590097c58f8c668f8c1
marcomeraf <marc.merafina@gmail.com>
2025-05-09 17:41:24 +0200

    Add safety checks for solarPanelConfigs access

--- src/routes/components/InputPanelsCount.svelte
+++ src/routes/components/InputPanelsCount.svelte
@@ -25,24 +25,31 @@
     const target = event.target as MdSlider;
     configId = target.value ?? 0;
   }
+
+  $: isValid = solarPanelConfigs && 
+               solarPanelConfigs.length > 0 && 
+               configId >= 0 && 
+               configId < solarPanelConfigs.length;
 </script>
 
-<div>
-  <table class="table-auto w-full body-medium secondary-text">
-    <tr>
-      <td class="primary-text"><md-icon>solar_power</md-icon> </td>
-      <th class="pl-2 text-left">Panels count</th>
-      <td class="pl-2 text-right">
-        <span>{solarPanelConfigs[configId].panelsCount} panels</span>
-      </td>
-    </tr>
-  </table>
+{#if isValid}
+  <div>
+    <table class="table-auto w-full body-medium secondary-text">
+      <tr>
+        <td class="primary-text"><md-icon>solar_power</md-icon> </td>
+        <th class="pl-2 text-left">Panels count</th>
+        <td class="pl-2 text-right">
+          <span>{solarPanelConfigs[configId].panelsCount} panels</span>
+        </td>
+      </tr>
+    </table>
 
-  <md-slider
-    class="w-full"
-    value={configId}
-    min={0}
-    max={solarPanelConfigs.length - 1}
-    on:change={onChange}
-  />
-</div>
+    <md-slider
+      class="w-full"
+      value={configId}
+      min={0}
+      max={solarPanelConfigs.length - 1}
+      on:change={onChange}
+    />
+  </div>
+{/if}
